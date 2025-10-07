299f136ead5a29c749053c098e7097f9e828ef2f
jonathanp568 <jonathanpan0510@icloud.com>
2025-10-07 10:12:15 -0400

    4

--- app.py
+++ app.py
@@ -3,4 +3,8 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
-
+list = 0
+list = int(list)
+for i in range(14117):
+    print(data[list]["title"])
+    list += 1
--- test.py
+++ test.py
@@ -1,15 +1,25 @@
-parking = input("How many parking spaces?")
-parking = int(parking)
-lane1 = input("Spaces in the first lane?")
-lane2 = input("Spaces in the second lane?")
-same = 0
-num = 0
-for i in range(parking):
-    if lane1[num] and lane2[num] == "C":
-        num += 1
-        same += 1
-    else:
-        num += 1
-print(same)
+# parking = input("How many parking spaces?")
+# parking = int(parking)
+# lane1 = input("Spaces in the first lane?")
+# lane2 = input("Spaces in the second lane?")
+# same = 0
+# num = 0
+# for i in range(parking):
+#     if lane1[num] and lane2[num] == "C":
+#         num += 1
+#         same += 1
+#     else:
+#         num += 1
+# print(same)
 
-
+def spaces(x,y):
+    occupied = 0
+    lane = 0
+    for char in x:
+        if x[lane] == "C" and y[lane] == "C":
+            occupied += 1
+            lane += 1
+        else:
+            lane += 1
+    print(occupied)
+spaces("C.CC.", "CCCCC")
