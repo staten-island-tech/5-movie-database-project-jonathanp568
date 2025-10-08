d967020fc3685ace8710714d7eaaac0d99143572
jonathanp568 <jonathanpan0510@icloud.com>
2025-10-08 10:17:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,56 @@ import json
 movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
+
+# list = 0
+# for i in range(14117):
+#     print(data[list]["title"])
+#     list += 1
+
+# list = 0
+# prompt = input("Movies after ____")
+# prompt = int(prompt)
+# while list != 14117:
+#     if data[list]["year"] > prompt:
+#         print(data[list]["title"])
+#         list += 1
+#     else: 
+#         list += 1
+
+# list = 14116
+# prompt = input("Movies before ____")
+# prompt = int(prompt)
+# while list != -1:
+#     if data[list]["year"] < prompt:
+#         print(data[list]["title"])
+#         list -= 1
+#     else: 
+#         list -= 1
+
+# list = 0
+# prompt = input("Movies during ____")
+# prompt = int(prompt)
+# while list != 14117:
+#     if data[list]["year"] == prompt:
+#         print(data[list]["title"])
+#         list += 1
+#     else: 
+#         list += 1
+
+# list = 0
+# prompt = input("Search for a movie")
+# while list != 14117:
+#     if prompt == data[list]["title"]:
+#         print(data[list])
+#         break
+#     else:
+#         list += 1
+
 list = 0
-list = int(list)
-for i in range(14117):
-    print(data[list]["title"])
-    list += 1
+prompt = input("Movie genre:")
+while list != 14117:
+    if prompt == data[list]["genres"]:
+        print(data[list]["title"])
+        list += 1
+    else:
+        list += 1
