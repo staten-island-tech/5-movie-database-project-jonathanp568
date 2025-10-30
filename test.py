f3334858bab620a741aeebb79d2bea144a8587cf
jonathanp568 <jonathanpan0510@icloud.com>
2025-10-30 10:08:55 -0400

    s

--- test.py
+++
@@ -1,8 +0,0 @@
-class pet():
-    def __init__(self, food):
-        self.food = food
-        return self.food
-    def feed(self, feed):
-        self.feed = feed 
-        return self.food + self.feed
-pet.__init__(10)
